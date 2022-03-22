0f35c7dfd562e93d0d6bd67eaeb3e98d0a61c3d3
ViniciusFarias <fsvinicius9@gmail.com>
2022-03-22 14:43:58 -0300

    consertando metodo adiciona no typescript

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,24 +1,24 @@
 import { Negociacao } from "../models/negociacao.js";
 
-export class NegociacaoController{
-  private inputData;
-  private inputQuantidade;
-  private inputValor;
+export class NegociacaoController {
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
 
-  constructor(){
+  constructor() {
     this.inputData = document.querySelector("#data");
     this.inputQuantidade = document.querySelector("#quantidade")
     this.inputValor = document.querySelector("#valor")
   }
 
-  adiciona(){
-   const negociacao = new Negociacao(
-     this.inputData.value,
-     this.inputQuantidade.value,
-     this.inputValor.value
-   );
+  adiciona() {
+    const exp = /-/g;
+    const date = new Date(this.inputData.value.replace(exp, ','));
+    const quantidade = parseInt(this.inputQuantidade.value)
+    const valor = parseFloat(this.inputValor.value)
+    const negociacao = new Negociacao(date, quantidade, valor);
 
-   console.log(negociacao);
-   
+    console.log(negociacao);
+
   }
 }
