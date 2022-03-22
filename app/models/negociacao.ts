9b3126a74777cf13d40f754bd268516e73fc3cc4
ViniciusFarias <fsvinicius9@gmail.com>
2022-03-22 15:40:49 -0300

    readonly e public ao mesmo tempo

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,17 +1,10 @@
 export class Negociacao {
-    constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
+    constructor(
+        public readonly _data: Date, 
+        public readonly _quantidade: number,  
+        public readonly _valor: number) {}
 
-    get data(): Date {
-        return this._data;
-    }
-
-    get quantidade() :number {
-        return this._quantidade;
-    }
-
-    get valor() :number {
-        return this._valor;
-    }
+        // deixando publico mas ninguem pode alterar
 
     get volume(): number {
         return this._quantidade * this._valor;
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -4,15 +4,7 @@ export class Negociacao {
         this._quantidade = _quantidade;
         this._valor = _valor;
     }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
-    }
+    // deixando publico mas ninguem pode alterar
     get volume() {
         return this._quantidade * this._valor;
     }
